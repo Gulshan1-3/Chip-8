56b76e8541c13b1bebc3ed6a2dd5d442b8737d8a
Gulshan1-3 <ankitstar955@gmail.com>
2024-05-28 16:33:56 +0530

    impl

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+
+pub const SCREEN_WIDTH:usize = 64;
+pub const SCREEN_HEIGHT:usize = 32;
+
+const RAM_SIZE:usize = 4096;
+const NUM_REGS:usize = 16;
+const STACK_SIZE:usize = 16;
+const NUMS_KEYS:usize = 16;
+const START_ADDR:u16 = 0x200;
+
+
+pub struct Emu{
+    pc: u16,
+    ram:[u8;RAM_SIZE],
+    screen:[bool;SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_regs:[u8;NUM_REGS], //used for faster op
+    i_regs: u16, //used for indexing into ram for read n write
+    sp:u16,
+    stack:[u16;STACK_SIZE],
+    keys:[bool;NUMS_KEYS],
+    dt:u8,
+    st:u8,
+
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Emu{
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn new()->Self {
+        Self { 
+            pc: START_ADDR,
+            ram:[0;RAM_SIZE],
+            screen:[false;SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_regs:[0;NUM_REGS], 
+            i_regs: 0, 
+            sp:0,
+            stack:[0;STACK_SIZE],
+            keys:[false;NUMS_KEYS],
+            dt:0,
+            st:0,
+            }
+        }
+       
+        fn push(&mut self,val:u16){
+         self.stack[self.sp as usize] = val;
+         self.sp+=1;
+        }
+
+        fn pop(&mut self)->u16{
+            self.sp-=1;
+            self.stack[self.sp as usize]
+        }
+
     }
-}
+
+
